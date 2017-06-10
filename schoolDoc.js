3c4680cf00f8b027a978e2e327cb712d6d548518
jameshornung <hornung.james@gmail.com>
2017-06-10 16:42:39 -0500

    beginning of school doc

---
+++ schoolDoc.js
@@ -0,0 +1,34 @@
+
+{
+	"universityName": "The University of Texas at Austin",
+	"campusLocation": "Austin",
+	"classLocationAddress": "1450 Willie Nelson Blvd",
+	"SSM": "Brett Payne",
+	"programs": ["FSF", "Data", "UI/UX"],
+	"cohorts": {
+		"FSF": [
+		{
+			"startDate": "June 3, 2017",
+			"endDate": "December 10, 2017",
+			"cohortName": "UT201706FSF",
+			"numberOfClasses": 2,
+			"instructorRoles": 2,
+			"taRoles": 4
+		},{
+			"startDate": "June 3, 2017",
+			"endDate": "December 10, 2017",
+			"cohortName": "UT201706FSF",
+			"numberOfClasses": 2,
+			"instructorRoles": 2,
+			"taRoles": 4
+		}],
+		"Data": [
+		{
+			"startDate": "June 3, 2017",
+			"endDate": "December 10, 2017",
+			"cohortName": "UT201706FSF-Data",
+			"numberOfClasses": 1,
+			"instructorRoles": 1,
+			"taRoles": 3
+		}
+		]
