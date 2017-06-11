42848a1fe6ace819e38598c9bccc40e231a05319
jameshornung <hornung.james@gmail.com>
2017-06-11 16:11:09 -0500

    basic structure for user doc

---
+++ userDoc.js
@@ -0,0 +1,19 @@
+//This is where we will store user data for TRTL.  This will allow employees to log in.  We can use this to restrict access and control who has read only priviledges and who is able to update info (and what info).  For example, this can allow an SSM to log in and see the role count, but not be able to add or remove candidates.  This should allows us to be able to protect the integrity of our data by controlling who is able to make updates.
+
+{
+	"username": "jedwoodarek",
+	"password": "iAmMadlyInLoveWithRomo8",
+	"accessLevel": "superUser"
+},{
+	"username": "jessica",
+	"password": "margsAllDayEveryDay99",
+	"accessLevel": "superUser"
+},{
+	"username": "un-namedSSM",
+	"password": "password1",
+	"accessLevel": "ssm"
+},{
+	"username": "josh-i-will-murder-you-on-my-island-poffenberger",
+	"password": "noles4EVAH",
+	"accessLevel": "superUser"
+};
